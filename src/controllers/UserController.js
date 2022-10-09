bcaad3f932c26e9443fbfb8418cdd84a51fb8488
Ana Freitas <ana.freitaas16@gmail.com>
2022-10-09 15:40:19 -0300

    users create controller

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -2,9 +2,23 @@ const knex = require('../database');
 
 module.exports = {
 
-    async index(req, res){
+    async select(req, res){
         const results = await knex('users')
 
         return res.json(results)
+    },
+
+    async create(req, res, next) {
+        try {
+            const { username } = req.body
+
+            await knex('users').insert({
+                username
+            })
+
+            return res.status(201).send()
+        } catch (error) {
+            next(error)
+        }
     }
 };
