53d1448424c925fc24f4098272b6ff0a4f0d3aaf
Ana Freitas <ana.freitaas16@gmail.com>
2022-10-09 16:00:46 -0300

    put router

--- src/routes.js
+++ src/routes.js
@@ -7,5 +7,6 @@ const UserController = require('./controllers/UserController');
 
 routes.get('/users', UserController.select);
 routes.post('/users', UserController.create);
+routes.put('/users/:id', UserController.update)
 
 module.exports = routes;
