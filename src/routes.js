67c35a61e292936b90085839975448497a00e6c1
Ana Freitas <ana.freitaas16@gmail.com>
2022-10-10 11:10:40 -0300

    get projects route

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,17 @@
 const express = require('express');
+const ProjectsCrontroller = require('./controllers/ProjectsCrontroller');
 
 const routes = express.Router();
 
 const UserController = require('./controllers/UserController');
 
-
+// users routes
 routes.get('/users', UserController.select);
 routes.post('/users', UserController.create);
 routes.put('/users/:id', UserController.update);
 routes.delete('/users/:id', UserController.delete);
+
+// 
+routes.get('/projects', ProjectsCrontroller.select);
 
 module.exports = routes;
