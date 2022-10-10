2be8b62ca1f61db141201c7427b1c6cb75cd1ae2
Ana Freitas <ana.freitaas16@gmail.com>
2022-10-10 12:05:07 -0300

    create projects controller

--- src/controllers/ProjectsCrontroller.js
+++ src/controllers/ProjectsCrontroller.js
@@ -5,15 +5,25 @@ module.exports = {
   async select(req, res, next) {
     try {
     
-        const { user_id } = req.query;
-        const query = knex("projects");
+        const { user_id, page =1 } = req.query;
+        const query = knex("projects")
+        .limit(5)
+        .offset((page -1) * 5)
+
+        const countObj = knex('projects').count()
 
         if (user_id) {
             query
             .where({user_id: user_id})
             .join('users', 'users.id', '=', 'projects.user_id')
             .select('projects.*', 'users.username')
+
+            countObj
+            .where({user_id: user_id})
         }
+
+        const [count] = await countObj
+        res.header('x-total-count', count["count"])
 
         const results = await query
         return res.json(results);
@@ -22,5 +32,18 @@ module.exports = {
       next(error);
     }
   },
+
+  async create(req, res, next) {
+    try {
+        const { title, user_id } = req.body;
+
+        await knex('projects')
+        .insert({ title, user_id })
+
+        return res.status(201).send()
+    } catch (error) {
+        next(error)
+    }
+}
 
 };
